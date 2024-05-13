4c795cd2861096712d060dd3cad579d26f26c8b3
Peaceteddie <Peaceteddie@gmail.com>
2024-05-13 17:46:19 +0200

    feat: now searches for company, and uses it if found, before deciding if form source should be used

--- Models/JobSource.cs
+++ Models/JobSource.cs
@@ -1,9 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Applied2Reminder;
 
 public class JobSource
 {
-    public int Id { get; set; }
-    public required string Name { get; set; }
+    [Key] public required string Name { get; set; }
     public JobSources SourceType { get; set; } = 0;
     public string? Description { get; set; }
     public string? Url { get; set; }
